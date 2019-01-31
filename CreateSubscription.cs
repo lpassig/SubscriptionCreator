fa6dd811325bac796937356dcfebcd60b10fd64e
lpassig <lp@propassig.de>
2019-01-31 16:47:35 +0100

    Updated Code

--- CreateSubscription.cs
+++ CreateSubscription.cs
@@ -8,6 +8,15 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
+// Azure dependencies
+using Microsoft.Rest.ClientRuntime;
+using Microsoft.Rest.Azure.Authentication;
+using Microsoft.Azure.Management.Billing;
+using Microsoft.Azure.Management.Billing.Models;
+using Microsoft.Azure.Management.Subscription;
+using Microsoft.Azure.Management.Subscription.Models;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
 namespace Company.Function
 {
     public static class CreateSubscription
@@ -30,4 +39,14 @@ namespace Company.Function
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
     }
+}
+public static async Task<string> GetToken(string resource, string apiversion)
+{
+  string msiEndpoint = Environment.GetEnvironmentVariable("MSI_ENDPOINT");
+  string endpoint = $"{msiEndpoint}/?resource={resource}&api-version={apiversion}";
+  string msiSecret = Environment.GetEnvironmentVariable("MSI_SECRET");
+  tokenClient.DefaultRequestHeaders.Add("Secret", msiSecret);
+  JObject tokenServiceResponse = JsonConvert
+      .DeserializeObject<JObject>(await tokenClient.GetStringAsync(endpoint));
+  return tokenServiceResponse["access_token"].ToString();
 }
